1c0980c2d046d250abefef0850ef3824cc485829
advancesoftcorp <39815078+advancesoftcorp@users.noreply.github.com>
2018-06-05 11:28:34 +0900

    move po file

--- docs/source/build.bat
+++ docs/source/build.bat
@@ -1,4 +1,4 @@
-msgcat ../locale/en/LC_MESSAGES/*.po > locale/en.po
+msgcat ../locale/en/LC_MESSAGES/*.po > ../locale/en.po
 sphinx-intl build
 sphinx-build -M html . ..\locale\ja -D language=ja
 sphinx-build -M html . ..\locale\en -D language=en
