bdada1fdd21bd9b6ad255af993d8210b910bdbac
advancesoftcorp <39815078+advancesoftcorp@users.noreply.github.com>
2018-06-21 20:28:23 +0900

    change theme and sidebar content

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,7 +75,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -96,7 +96,7 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+html_sidebars = {'**':['globaltoc.html', 'searchbox.html']}
 
 html_favicon = 'img/favicon.ico'
 
